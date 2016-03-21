a6fc3863d775997e8d43b732897be9f889535638
Jipih <jeanphilippe.pinon@gmail.com>
2016-03-21 16:38:10 +0100

    ajout de la classe livraison

---
+++ visual/ClassLibrary1/livr dao.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+   public class livr
+    {
+        SqlConnection con = new SqlConnection("server=.; database=shop; integrated security=true");
+        public List<livraison> Liv()
+        {
+            List<livraison> liste = new List<livraison>();
+
+            con.Open();
+
+            SqlCommand requete = new SqlCommand("select * from Livraison where Date_Livraison is null", con);
+
+            SqlDataReader resultat = requete.ExecuteReader();
+
+            while (resultat.Read())
+            {
+                livraison liv = new livraison();
+                liv.Bon_Livraison = Convert.ToInt32(resultat["Bon_Livraison"]);
+                liv.Liste_Produit = Convert.ToString(resultat["Liste_Produit"]);
+                liv.Adr_Livraison = Convert.ToString(resultat["Adr_Livraison"]);
+
+                liv.Date_Livraison = 0;
+                if (resultat["Date_Livraison"] != DBNull.Value)
+                {
+                    liv.Date_Livraison = Convert.ToInt32(resultat["Date_Livraison"]);
+                }
+
+                liste.Add(liv);
+            }
+            con.Close();
+            return liste;
+        }
+
+        }
+    }
